0601ac73c5fb45f3413759f7759cadc80fa222da
David Story <dstory@questcontrols.com>
2019-11-03 05:05:54 -0500

    Updates

---
+++ precache-manifest.65c37b9051a7b20dfdd1bf859e5950a0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5127b7341ac91ff317ab1e57537532ba",
+    "url": "/marvel-comics/index.html"
+  },
+  {
+    "revision": "07b5c6b5c3087ca78f9a",
+    "url": "/marvel-comics/static/css/2.c4ca04e2.chunk.css"
+  },
+  {
+    "revision": "bc95bef301587df7dedc",
+    "url": "/marvel-comics/static/css/main.b42a912a.chunk.css"
+  },
+  {
+    "revision": "07b5c6b5c3087ca78f9a",
+    "url": "/marvel-comics/static/js/2.620cf3c4.chunk.js"
+  },
+  {
+    "revision": "bc95bef301587df7dedc",
+    "url": "/marvel-comics/static/js/main.6a05c97c.chunk.js"
+  },
+  {
+    "revision": "2442f258ddaff5c9f880",
+    "url": "/marvel-comics/static/js/runtime-main.71606f63.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/marvel-comics/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/marvel-comics/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/marvel-comics/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/marvel-comics/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/marvel-comics/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/marvel-comics/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/marvel-comics/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/marvel-comics/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/marvel-comics/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/marvel-comics/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/marvel-comics/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/marvel-comics/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/marvel-comics/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/marvel-comics/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/marvel-comics/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/marvel-comics/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
